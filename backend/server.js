1f99cf53599f99d935cad11b129c6070fb2f5edb
MohammedBasha <me@mohammedbasha.com>
2026-01-24 18:15:19 +0200

    Finishing the User route and controller

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
-import authRoutes from "./routes/auth.routes.js";
+import { V2 as cloudinary } from "cloudinary";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +22,7 @@ app.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
